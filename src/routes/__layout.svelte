34cc97674c6ca2e18f2017d6940cefc6304a22af
Ali Sharafi <ali.sharafi.github@gmail.com>
2022-07-24 23:56:26 +0430

    feat: reset layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,66 @@
 <script>
 	import 'carbon-components-svelte/css/all.css';
+
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		SideNav,
+		SideNavItems,
+		SideNavMenu,
+		SideNavMenuItem,
+		SideNavLink,
+		SideNavDivider,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column
+	} from 'carbon-components-svelte';
+	import Fade from 'carbon-icons-svelte/lib/Fade.svelte';
+
+	let isSideNavOpen = false;
 </script>
 
-<slot />
+<Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/" text="Link 1" />
+		<HeaderNavItem href="/" text="Link 2" />
+		<HeaderNavItem href="/" text="Link 3" />
+		<HeaderNavMenu text="Menu">
+			<HeaderNavItem href="/" text="Link 1" />
+			<HeaderNavItem href="/" text="Link 2" />
+			<HeaderNavItem href="/" text="Link 3" />
+		</HeaderNavMenu>
+		<HeaderNavItem href="/" text="Link 4" />
+	</HeaderNav>
+
+	<SideNav bind:isOpen={isSideNavOpen} rail>
+		<SideNavItems>
+			<SideNavLink icon={Fade} text="Link 1" href="/" isSelected />
+			<SideNavLink icon={Fade} text="Link 2" href="/" />
+			<SideNavLink icon={Fade} text="Link 3" href="/" />
+			<SideNavMenu icon={Fade} text="Menu">
+				<SideNavMenuItem href="/" text="Link 1" />
+				<SideNavMenuItem href="/" text="Link 2" />
+				<SideNavMenuItem href="/" text="Link 3" />
+			</SideNavMenu>
+			<SideNavDivider />
+			<SideNavLink icon={Fade} text="Link 4" href="/" />
+		</SideNavItems>
+	</SideNav>
+</Header>
+
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<slot />
+			</Column>
+		</Row>
+	</Grid>
+</Content>
